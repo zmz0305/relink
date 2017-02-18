34cc203503e2178b2523fcec5bf42d16039ab1d0
Songyu Li <songyuli@Songyus-Mac-mini.local>
2017-02-17 21:23:46 -0600

    added socket room

--- chat_service/internal/room_data.js
+++ chat_service/internal/room_data.js
@@ -3,6 +3,30 @@
  */
 // example
 // {'roomcode': [socketid1, socketid2, socketid3...]}
-module.exports.rooms = {
-    // room_id: 1 (count of people in this room)
-};
+/**
+ * Created by zmz0305 on 2/11/17.
+ */
+// example
+// {'roomcode': [socketid1, socketid2, socketid3...]}
+var room_plp = {}
+var room_arr = {}
+exports.add_room = function(room_name, socketid){
+  room_plp[room_name] = 1
+  room_arr[room_name] = new Array()
+  room_arr[room_name].push(socketid)
+}
+
+exports.join_room = function(room_name, socketid){
+  //if()
+  room_arr[room_name].push(socketid)
+  room_plp[room_name] += 1
+}
+
+exports.check_room = function(room_name){
+  if(room_arr[room_name] != null){
+    return true
+  }
+  return false
+}
+
+exports.debug_info = {room_plp, room_arr}
