89432d1e2c716e7fe02995fb18b646dde77a6556
Mukesh <mukgan9@gmail.com>
2017-02-19 11:24:06 -0600

    Added post request

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,4 +1,28 @@
 $(document).ready(
 	function(){
-    alert("Hello");
-                  });
+		$( "#register" ).submit(function( event ) {
+			event.preventDefault();
+			
+			$.ajax({
+			  type: "POST",
+			  url: "http://127.0.0.1:8000/accounts/register",
+			  data: {
+			  	firstname: $('#rFirstName').val(),
+			  	lastname: $("#rLastName").val(),
+			  	password: $("#rPassword").val(),
+			  	username: $("#rEmail").val()
+			  },
+			  success: function(data) {
+			  	console.log("success");
+			  },
+			  error: function(data) {
+			  	console.log(data);
+			  },
+			  complete: function(data) {
+			  	console.log(data);
+			  }
+			});
+
+			
+		});
+});
