cd2810e1c46645169c325edede0beb80ecf967fa
Songyu Li <lisongyu2009@gmail.com>
2017-04-22 17:00:41 -0500

    Update Room.jsx

--- frontend/scenes/Room.jsx
+++ frontend/scenes/Room.jsx
@@ -118,6 +118,7 @@ export default class Room extends React.Component {
         return (
             <div>
                 <Jumbotron>
+                    <h3>{this.state.roomId}</h3>
                 <div style={styleChat}>
                 <ListGroup>
                 {messages}
